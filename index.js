1eb70acdeb94d8b9785dc983999e1e7e26654755
ridoarizzzz <juragan.zoro19@gmail.com>
2026-05-22 13:48:43 +0700

    index.js2

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const {
     useMultiFileAuthState,
     downloadContentFromMessage,
     emitGroupParticipantsUpdate,
-    emitGroupUpdate,
     generateWAMessageContent,
     generateWAMessage,
     makeInMemoryStore,
